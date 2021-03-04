892cd8a179587e84c2a208c8b8a8bf03ad67fcd9
Sultan Alsawaf <sultan@kerneltoast.com>
2021-03-04 21:48:52 +0530

    msm: kgsl: Remove L2PC PM QoS feature
    KGSL already has PM QoS covering what matters. The L2PC PM QoS code is
    not only unneeded, but also unused, so remove it. It's poorly designed
    anyway since it uses a timeout with PM QoS, which is drastically bad for
    power consumption.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    
    	modified:   Documentation/devicetree/bindings/gpu/adreno.txt
    	modified:   drivers/gpu/msm/adreno.c
    	modified:   drivers/gpu/msm/adreno_dispatch.c
    	modified:   drivers/gpu/msm/kgsl.c
    	modified:   drivers/gpu/msm/kgsl_pwrctrl.c
    	modified:   drivers/gpu/msm/kgsl_pwrctrl.h

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -888,11 +888,6 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 		&device->pwrctrl.pm_qos_active_latency))
 		device->pwrctrl.pm_qos_active_latency = 501;
 
-	/* get pm-qos-cpu-mask-latency, set it to default if not found */
-	if (of_property_read_u32(node, "qcom,l2pc-cpu-mask-latency",
-		&device->pwrctrl.pm_qos_cpu_mask_latency))
-		device->pwrctrl.pm_qos_cpu_mask_latency = 501;
-
 	/* get pm-qos-wakeup-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,pm-qos-wakeup-latency",
 		&device->pwrctrl.pm_qos_wakeup_latency))
@@ -1099,7 +1094,7 @@ static int adreno_remove(struct platform_device *pdev)
 
 #ifdef CONFIG_ADRENO_CORESNAP
 	adreno_coresight_remove(adreno_dev);
-#endif	
+#endif
 	adreno_profile_close(adreno_dev);
 
 	kgsl_pwrscale_close(device);
