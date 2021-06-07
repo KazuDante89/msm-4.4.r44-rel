47824c5fe27c0220f4cc7860f87b1f9d909e1d98
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-06-07 01:20:30 -0400

    Freq tweaks 	modified:   .circleci/build.sh 	modified:   arch/arm/boot/dts/qcom/sdm660.dtsi

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -57,7 +57,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [EAS]-KazuKernel_v.0.0.zip *
+    zip -r9 [EAS]-KazuKernel_v.0.1.zip *
     cd ..
 }
 sendinfo
