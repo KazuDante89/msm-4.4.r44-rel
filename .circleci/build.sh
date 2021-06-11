db46305e7b0cc9f96765ef4b3380d13fc47fffbd
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-06-11 12:05:48 -0400

    Slight changes/touchups 	modified:   .circleci/build.sh 	modified:   arch/arm/boot/dts/qcom/sdm660.dtsi 	modified:   kernel/sched/fair.c

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -57,7 +57,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [EAS]-Nebula_R3_v.0.0.1.zip *
+    zip -r9 [EAS]-Nebula_R3_v.0.0.2.zip *
     cd ..
 }
 sendinfo
