8df596ade1df15a53aa0a44c4fa254586b5c351e
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-06-11 23:39:01 -0400

    Testingeven moer commits 	modified:   .circleci/build.sh 	modified:   arch/arm64/configs/lavender-perf_defconfig

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -57,7 +57,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [R4]-Nebula_v.0.0.3.zip *
+    zip -r9 [R4]-Nebula_v.0.0.4.zip *
     cd ..
 }
 sendinfo
