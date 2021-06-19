ee380a49210b030a92e2866a57565a1131b36069
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-06-19 19:45:29 -0400

    Switch to slmk & other tweaks 	modified:   .circleci/build.sh 	modified:   arch/arm/boot/dts/qcom/sdm660.dtsi 	modified:   arch/arm64/configs/lavender-perf_defconfig 	modified:   mm/Kconfig 	modified:   mm/vmstat.c

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -57,7 +57,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [R5.5]-Nebula_v.0.1.zip *
+    zip -r9 [R5.5]-Nebula_v.0.2.zip *
     cd ..
 }
 sendinfo
