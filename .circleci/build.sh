fd7b2a4314aefa26d1bcde10632d10f35ccc2600
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-07-28 18:43:53 -0400

    more test 	modified:   .circleci/build.sh 	modified:   arch/arm/boot/dts/qcom/sdm660.dtsi

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -57,7 +57,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [R6]-Genshin_v.0.0.2.zip *
+    zip -r9 [R6]-Genshin_v.0.0.3.zip *
     cd ..
 }
 sendinfo
