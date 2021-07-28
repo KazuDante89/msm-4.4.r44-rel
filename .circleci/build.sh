4468e483a8dff633c79c3b59ee1473ac6d791b2c
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-07-28 18:02:42 -0400

    testing something 	modified:   .circleci/build.sh 	modified:   arch/arm/boot/dts/qcom/sdm660.dtsi 	modified:   arch/arm64/configs/lavender-perf_defconfig

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -57,7 +57,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [R6]-Genshin_v.0.0.1.zip *
+    zip -r9 [R6]-Genshin_v.0.0.2.zip *
     cd ..
 }
 sendinfo
